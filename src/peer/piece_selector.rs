12ff1e779171a4743bab14d81d3012224891c52e
Amitay Horwitz <amitayh@gmail.com>
2025-03-09 16:02:36 +0200

    wip

--- src/peer/piece_selector.rs
+++ src/peer/piece_selector.rs
@@ -1,5 +1,6 @@
 use std::cmp::Reverse;
 use std::collections::HashSet;
+use std::iter::{Cycle, Zip};
 use std::{collections::HashMap, net::SocketAddr};
 
 use bit_set::BitSet;
@@ -7,13 +8,14 @@ use priority_queue::PriorityQueue;
 use size::Size;
 use tokio::sync::Notify;
 
+use crate::peer::blocks::Blocks;
 use crate::peer::message::Block;
 
 #[allow(dead_code)]
 pub struct PieceSelector {
     piece_to_peers: HashMap<usize, HashSet<SocketAddr>>,
     rarest_piece: PriorityQueue<usize, Reverse<usize>>,
-    current_piece: Option<usize>,
+    current_piece: Option<Zip<Cycle<std::vec::IntoIter<SocketAddr>>, Blocks>>,
     piece_size: Size,
     total_size: Size,
     block_size: Size,
@@ -36,7 +38,14 @@ impl PieceSelector {
         }
     }
 
-    async fn next(&mut self) -> Option<(&SocketAddr, Block)> {
+    async fn next(&mut self) -> Option<(SocketAddr, Block)> {
+        if let Some(blocks) = &mut self.current_piece {
+            if let Some((addr, block)) = blocks.next() {
+                return Some((addr, block));
+            } else {
+                self.current_piece = None;
+            }
+        }
         // get rarest pieces that i don't have
         // sort peers by transfer rate
         // assign blocks to available peers
@@ -47,14 +56,22 @@ impl PieceSelector {
         while self.rarest_piece.is_empty() && self.completed_pieces.len() < total_pieces {
             self.notify.notified().await;
         }
-        while let Some((piece, _)) = self.rarest_piece.pop() {
-            let peer = self
+
+        if let Some((piece, _)) = self.rarest_piece.pop() {
+            let peers = self
                 .piece_to_peers
                 .get(&piece)
-                // TODO: select peer with best upload rate
-                .and_then(|peers| peers.iter().next())
                 .expect("at least 1 peer should have the piece");
-            return Some((peer, Block::new(piece, 0, self.block_size.bytes() as usize)));
+            // TODO: select peer with best upload rate
+            //.and_then(|peers| peers.iter().next())
+            let peers: Vec<_> = peers.iter().cloned().collect();
+            let peers_cycle = peers.into_iter().cycle();
+            let blocks = Blocks::new(self.piece_size, self.total_size, self.block_size, piece);
+            let mut zipped = peers_cycle.zip(blocks);
+            if let Some((addr, block)) = zipped.next() {
+                self.current_piece = Some(zipped);
+                return Some((addr, block));
+            }
         }
         None
     }
@@ -62,6 +79,13 @@ impl PieceSelector {
     fn piece_complete(&mut self, piece: usize) {
         self.completed_pieces.insert(piece);
         self.notify.notify_one();
+    }
+
+    // TODO: test
+    fn peer_disconnected(&mut self, addr: &SocketAddr) {
+        for peers in self.piece_to_peers.values_mut() {
+            peers.remove(addr);
+        }
     }
 
     fn peer_has_pieces(&mut self, addr: SocketAddr, pieces: BitSet) {
@@ -97,7 +121,7 @@ mod tests {
 
         assert_eq!(
             state.next().await,
-            Some((&addr, Block::new(0, 0, BLOCK_SIZE_BYTES)))
+            Some((addr, Block::new(0, 0, BLOCK_SIZE_BYTES)))
         );
     }
 
@@ -109,7 +133,7 @@ mod tests {
 
         assert_eq!(
             state.next().await,
-            Some((&addr, Block::new(1, 0, BLOCK_SIZE_BYTES)))
+            Some((addr, Block::new(1, 0, BLOCK_SIZE_BYTES)))
         );
     }
 
@@ -122,7 +146,7 @@ mod tests {
 
         assert_eq!(
             state.next().await,
-            Some((&addr, Block::new(1, 0, BLOCK_SIZE_BYTES)))
+            Some((addr, Block::new(1, 0, BLOCK_SIZE_BYTES)))
         );
     }
 
@@ -138,7 +162,7 @@ mod tests {
 
         assert_eq!(
             state.next().await,
-            Some((&addr2, Block::new(1, 0, BLOCK_SIZE_BYTES)))
+            Some((addr2, Block::new(1, 0, BLOCK_SIZE_BYTES)))
         );
     }
 
@@ -151,12 +175,12 @@ mod tests {
 
         assert_eq!(
             state.next().await,
-            Some((&addr, Block::new(0, 0, BLOCK_SIZE_BYTES)))
+            Some((addr, Block::new(0, 0, BLOCK_SIZE_BYTES)))
         );
-        //assert_eq!(
-        //    state.next().await,
-        //    Some((&addr, Block::new(0, BLOCK_SIZE_BYTES, BLOCK_SIZE_BYTES)))
-        //);
+        assert_eq!(
+            state.next().await,
+            Some((addr, Block::new(0, BLOCK_SIZE_BYTES, BLOCK_SIZE_BYTES)))
+        );
     }
 
     // TODO: distribute blocks between peers, by upload rate
