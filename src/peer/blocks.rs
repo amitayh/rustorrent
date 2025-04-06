147c407b4030c194dbc22086f6956161d699edd0
Amitay Horwitz <amitayh@gmail.com>
2025-04-06 12:56:58 +0300

    implement `ExactSizeIterator` for `Blocks`

--- src/peer/blocks.rs
+++ src/peer/blocks.rs
@@ -33,6 +33,12 @@ impl Iterator for Blocks {
     }
 }
 
+impl ExactSizeIterator for Blocks {
+    fn len(&self) -> usize {
+        ((self.end as f64) / (self.block_size as f64)).ceil() as usize
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,6 +47,7 @@ mod tests {
     fn one_piece_one_block() {
         let mut blocks = Blocks::new(0, 1024, 1024);
 
+        assert_eq!(blocks.len(), 1);
         assert_eq!(Some(Block::new(0, 0, 1024)), blocks.next());
         assert_eq!(None, blocks.next());
     }
@@ -49,6 +56,7 @@ mod tests {
     fn one_piece_multiple_blocks() {
         let mut blocks = Blocks::new(0, 2048, 1024);
 
+        assert_eq!(blocks.len(), 2);
         assert_eq!(Some(Block::new(0, 0, 1024)), blocks.next());
         assert_eq!(Some(Block::new(0, 1024, 1024)), blocks.next());
         assert_eq!(None, blocks.next());
@@ -58,6 +66,7 @@ mod tests {
     fn uneven_block_sizes() {
         let mut blocks = Blocks::new(0, 1066, 1024);
 
+        assert_eq!(blocks.len(), 2);
         assert_eq!(Some(Block::new(0, 0, 1024)), blocks.next());
         assert_eq!(Some(Block::new(0, 1024, 42)), blocks.next());
         assert_eq!(None, blocks.next());
@@ -67,6 +76,7 @@ mod tests {
     fn uneven_block_sizes_in_non_first_piece() {
         let mut blocks = Blocks::new(1, 1066, 1024);
 
+        assert_eq!(blocks.len(), 2);
         assert_eq!(Some(Block::new(1, 0, 1024)), blocks.next());
         assert_eq!(Some(Block::new(1, 1024, 42)), blocks.next());
         assert_eq!(None, blocks.next());
@@ -76,6 +86,7 @@ mod tests {
     fn uneven_block_sizes_in_last_piece() {
         let mut blocks = Blocks::new(1, 1024 * 3, 1024);
 
+        assert_eq!(blocks.len(), 3);
         assert_eq!(Some(Block::new(1, 0, 1024)), blocks.next());
         assert_eq!(Some(Block::new(1, 1024, 1024)), blocks.next());
         assert_eq!(Some(Block::new(1, 2048, 1024)), blocks.next());
@@ -83,6 +94,7 @@ mod tests {
 
         let mut blocks = Blocks::new(2, 1024 * 2, 1024);
 
+        assert_eq!(blocks.len(), 2);
         assert_eq!(Some(Block::new(2, 0, 1024)), blocks.next());
         assert_eq!(Some(Block::new(2, 1024, 1024)), blocks.next());
         assert_eq!(None, blocks.next());
@@ -92,6 +104,7 @@ mod tests {
     fn real_world_example() {
         let mut blocks = Blocks::new(5, 6760, 16384);
 
+        assert_eq!(blocks.len(), 1);
         assert_eq!(Some(Block::new(5, 0, 6760)), blocks.next());
         assert_eq!(None, blocks.next());
     }
